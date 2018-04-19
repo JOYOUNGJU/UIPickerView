32466aaff4d68e46ec68a2cd53d94f8b22af5b93
youngju <asdadssad@naver.coim>
2018-04-19 15:05:46 +0900

    add

--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var myLable: UILabel!
+    @IBOutlet weak var myLable2: UILabel!
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 10
+        }else{
+            return 100
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        
+        if component == 0 {
+            return "comp01 \(row)"
+        } else {
+            return "comp02 \(row)"
+        }
     }
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        if component == 0 {
+            myLable.text = "comp01 \(row)"
+        } else {
+     myLable2.text = "comp02 \(row)"
+        }
+        
+    }
 
 }
 
